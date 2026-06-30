c462693b6aace78e8f306c8831d4f4bf7597a6d7
Your Name <maherin2114@gmail.com>
2026-06-30 08:11:28 +0530

    basic printing

---
+++ JSFOUNDATION/part1/printing.js
@@ -0,0 +1,10 @@
+console.log("Hello World!!")
+console.log("Hiii!")
+
+process.stdout.write("helloo")  
+process.stdout.write("world")   //doesnt prints on another line
+
+console.table({city: "Mumbai"})
+console.table({city: "Banglore"})
+
+console.warn({city: "delhi"}) //prints in some fancy type-- colours
