2d61be6d207a9134d98d93167f4122924e282234
Your Name <maherin2114@gmail.com>
2026-07-01 08:30:16 +0530

    arithmetic operations

---
+++ JSFOUNDATION/part1/operations.js
@@ -0,0 +1,32 @@
+let score = 102
+let bonus = 25
+
+let totalScore = score + bonus
+
+console.log(totalScore)
+
+let addition = 4 + 5
+let subtraction = 10 - 5
+let mult = 5 * 3
+let divi = 8 / 2
+let remainder = 9 % 2
+let expo = 2 ** 3 //2*2*2
+
+let myscore = 100
+myscore++ //increment by 1
+console.log(myscore)
+
+let credits = 100
+credits-- //decrement by 1
+console.log(credits)
+
+//comparison operation
+
+let num1 = 3
+let num2 = 3
+let num3 = 6
+
+console.log("value:", num1 == num2) //true
+console.log(num1 != num3) //true
+console.log(num1 > num3) //false
+console.log(num1 < num3) //true
