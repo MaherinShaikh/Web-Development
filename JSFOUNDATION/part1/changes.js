e097fe5535afe227b40280e66b76043832c49186
Your Name <maherin2114@gmail.com>
2026-07-01 08:17:13 +0530

    datatypes

---
+++ JSFOUNDATION/part1/changes.js
@@ -0,0 +1,11 @@
+let gameName = "cricket"
+
+gameName = "football"  //reassigning the value of gameName
+
+console.log(gameName)
+
+const username = "john" //cannot assign a new value to username
+
+// username = "doe" //this will give an error
+
+console.log(username)
---
+++ JSFOUNDATION/part1/datatypes.js
@@ -0,0 +1,42 @@
+console.log("hiee");
+
+// console.log("world").   //this is a comment. use command + / to comment in vs code
+
+/*
+ multiline comments
+*/
+
+/*
+
+    Strings
+    Numbers
+    Boolean - yes or no
+    Bigint - used for very large number
+
+    undefined - no def value yet but there will be in future or maybe not
+    null - empty - doesnt mean 0 
+
+    object 
+
+    symbol - make something really unique
+
+*/
+
+// var- just like a placeholder
+
+// var score = 100   //old way
+
+let score = 102 
+let name = "john"
+let isloggedin = false
+
+//object - collection like array
+
+let teaTypes = ["lemon tea", "green tea", "orange tea"]
+let user = {firstname: "john", lastname: "doe"}
+
+let getScore = score
+
+
+console.log(getScore)
+
