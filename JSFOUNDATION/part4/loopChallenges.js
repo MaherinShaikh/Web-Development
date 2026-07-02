d5cbd8d920ad8aa988551b6cb5db0a01184efd1b
Your Name <maherin2114@gmail.com>
2026-07-02 13:29:02 +0530

    Loop Challenges

--- JSFOUNDATION/part4/loopChallenges.js
+++ JSFOUNDATION/part4/loopChallenges.js
@@ -9,3 +9,65 @@ while (i <= 5) {
 }
 console.log(sum)
 
+/* 2. Write a 'while loop' that counts down from 5 to 1 and stores the numbers in an array named 'countdown' */
+
+let countdown = []
+let j = 5
+
+while (j > 0) {
+    countdown.push(j)
+    j--
+}
+console.log(countdown)
+
+/* 3. Write a 'do while loop' that prompts a user to enter their favourite tea type until they enter 'stop'.
+    store each tea type in an array named 'teaCollection' */
+
+//Run this code in browser console to see prompt in action
+let teaCollection = []
+let tea
+
+do {
+    tea = prompt('Enter your fav tea (type "stop" to finish)')
+
+    if (tea !== "stop") {
+        teaCollection.push(tea)
+    }
+
+} while (tea !== "stop");
+console.log(teaCollection)
+
+/* 4. Write a 'do while loop' that adds numbers from 1 to 3 and stores the result in a variable named 'total' */
+
+let total = 0
+let k = 1
+
+do {
+    total += k
+    k++    
+} while (k <= 3);
+console.log(total)
+
+/* 5. write a 'for loop' that multiplies each element in the array '[2, 4, 6]' by 2 and stores the results in a new array named 'multipliedNumbers'. */
+
+let arr = [2, 4, 6]
+let multipliedNumbers = []
+
+for (let l = 0; l < arr.length; l++) {
+    // newNumber = arr[l] * 2
+    // multipliedNumbers.push(newNumber)
+
+    multipliedNumbers.push(arr[l] * 2)
+}
+console.log(multipliedNumbers)
+
+/* 6. Write a 'for loop' that lists all the cities in the array '["Paris", "New York", "Tokyo", "London"]' and stores each city in a new array named 'cityList'. */
+
+let cities = ["Paris", "New York", "Tokyo", "London"]
+let cityList = []
+
+for (let c = 0; c < cities.length; c++) {
+    const myCity = cities[c];
+    cityList.push(myCity)
+}
+console.log(cityList)
